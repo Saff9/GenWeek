73ca4dbf045e3ef926e181de55012af0f300827e
Saff9 <saffanakbar942@gmail.com>
2025-10-26 12:53:32 +0530

    Create auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,45 @@
+import { getServerSession } from "next-auth";
+import { prisma } from "./prisma";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+        if (!user) return null;
+
+        const valid = await bcrypt.compare(credentials!.password, user.password);
+        if (!valid) return null;
+
+        return { id: user.id, email: user.email, name: user.name };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user as any;
+      return session;
+    },
+  },
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
