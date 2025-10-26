e2b9a78e6cec6dbded0ef0fea3a8b9436d592d70
Saff9 <saffanakbar942@gmail.com>
2025-10-26 12:39:11 +0530

    Create route.ts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!user) return null;
+
+        const valid = await bcrypt.compare(credentials!.password, user.password);
+        if (!valid) return null;
+
+        return user;
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
