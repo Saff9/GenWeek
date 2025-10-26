7f7bb22f56317a95b749e90199a554b1da897578
Saff9 <saffanakbar942@gmail.com>
2025-10-26 12:44:20 +0530

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-primary">
+      <span className="loader mb-3"></span>
+      <p>Loading awesome GenWeek vibes...</p>
+    </div>
+  );
+}
