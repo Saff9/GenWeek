862815a1c423d6b62117cde2e96b634c631c17c7
Saff9 <saffanakbar942@gmail.com>
2025-10-28 14:58:52 +0530

    Create QuickActions.tsx

---
+++ app/components/QuickActions.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function QuickActions(){
+  return (
+    <div className="card quick-actions">
+      <button>Live</button>
+      <button>Photo</button>
+      <button>Check in</button>
+    </div>
+  );
+}
